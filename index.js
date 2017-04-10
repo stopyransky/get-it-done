4e922e96e67064dc7d0f20330477aff2a61001c9
Karol Stopyra <stopyransky@gmail.com>
2017-04-10 22:05:09 +0200

    before weback

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use( express.static('public') );
+
+
+MongoClient.connect("mongodb://localhost:27017/scratch", function onConnect( err, db ) {
+
+} );
+
+
 app.listen( 3000, function() {
     console.log("Listening on port 3000.");
 } );
--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,58 @@
 
 var container = document.getElementById("container");
 
-// another method
+// presentational component
+var AppMessageComponent = React.createClass( {
+    
+    render : function() {
+        
+        var name = this.props.name;
+        var msg = this.props.msg;
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>{msg}</p>
+            </div>
+        );
+    } 
+} );
+
+// input handling component
+var AppFormComponent = React.createClass({
+    
+    onFormSubmit: function( e ) {
+        
+        e.preventDefault();
+        var updates = {};
+        var name = this.refs.name.value;
+        var msg  = this.refs.msg.value;
+        
+        if( name.length > 0 ) {
+            this.refs.name.value = ""; // clears text box
+            updates.name = name;
+        }
+        if( msg.length > 0) {
+            this.refs.msg.value ="";
+            updates.msg = msg;
+        }
+        
+        this.props.onUpdate( updates );
+    },
+
+    render : function() {
+        return (
+            <form onSubmit={ this.onFormSubmit }>
+            <input placeholder="Enter name: " type="text" ref="name"/>
+            <br>
+            <textarea placeholder="Enter message: " ref="msg"></textarea>
+            <br>
+            <button>Submit</button>
+            </form> 
+        );
+    } 
+});
+
+//container component
 var AppComponent = React.createClass( {
 
     getDefaultProps : function() {
@@ -10,31 +61,33 @@ var AppComponent = React.createClass( {
             msg : 'This is default message from component.'
         }
     },
-    handleSubmitButton : function( e ) {
-        e.preventDefault();
 
-        var stuff = this.refs.stuff.value;
-        alert(stuff);
+    getInitialState : function() {
+        return {
+            name : this.props.name,
+            msg : this.props.msg
+        }
     },
+
+    handleUpdates  : function( updates ) {
+        this.setState( updates );
+    },
+
     render : function() {
-        var name = this.props.name;
-        var msg = this.props.msg;
+        //var name = this.props.name;
+        var name = this.state.name;
+        var msg = this.state.msg;
         return (
             <div>
-                <h1>Hello {name}!</h1>
-                <p>{msg}</p>
-                <form onSubmit={this.handleSubmitButton}>
-                    <input type="text" ref="stuff"/>
-                    <button>Submit</button>
-                </form>
+                <AppMessageComponent name={name} msg={msg} />
+                <AppFormComponent onUpdate={this.handleUpdates} />
             </div>
         );
     }
 } );
 
 var firstName = 'Karol';
-var message;
-message = 'Custom message for Karol.';
+var message = 'Custom message for ' + firstName;
 ReactDOM.render( <AppComponent name={firstName} msg={message}/>, container );
 
 // another method :
