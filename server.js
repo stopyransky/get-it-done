3602ca96a0e38b3f022b86fe302e255821753999
Karol Stopyra <stopyransky@gmail.com>
2017-04-15 16:31:36 +0200

    prepared for heroku

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-var express = require('express'),
-	MongoClient = require('mongodb').MongoClient;
+var express = require('express');
+	//MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use( function( req, res, next ) {
+	if( req.headers['x-forwarded-proto'] === "http") {
+		next();
+	} else {
+		res.redirect("http://" + req.hostname + req.url );
+	}
+});
 
 app.use( express.static('public') );
 
 
-MongoClient.connect("mongodb://localhost:27017/scratch", function onConnect( err, db ) {
+// MongoClient.connect("mongodb://localhost:27017/scratch", function onConnect( err, db ) {
 
-} );
+// } );
 
 
-var server = app.listen( 3000, function() {
-	var port = server.address().port;
-    console.log("Listening on port %s.", port);
+var server = app.listen( PORT, function() {
+	// var port = server.address().port;
+    console.log("Listening on port %s.", PORT);
 } );
