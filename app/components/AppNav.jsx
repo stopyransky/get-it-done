7735b11348ffffb4de548dbf5386af0e902ca843
Karol Stopyra <stopyransky@gmail.com>
2017-04-17 22:40:26 +0200

    style nav bar

--- app/components/AppNav.jsx
+++ app/components/AppNav.jsx
@@ -1,12 +1,53 @@
 var React = require("react");
 var {Link, IndexLink} = require("react-router");
 
-var AppNav = ( props ) =>
+var AppNav = React.createClass({
+	onSearch : function(e ) {
+		e.preventDefault();
+		alert('not yet wired up');
+	},
+	render : function() {
+		return (
+		<div className ="top-bar">
+			<div className="top-bar-left">
+				<ul className ="menu">
+					<li className="menu-text">React App!</li>
+					<li>
+						<IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Greeter</IndexLink>
+					</li>
+					<li>
+						<Link to="/weather" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Weather</Link>
+					</li>
+					<li>
+						<Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+			<form onSubmit={this.onSearch}>
+				<ul className="menu">
+					<li>
+						<input type="search" placeholder="Search weather"/>
+					</li>
+					<li>
+						<input type="submit" className="button" value="Get Weather"/>
+					</li>
+				</ul>
+			</form>
+			</div>
+		</div>
+		);
+	}
+});
+
+	
+module.exports = AppNav;
+
+var old = (
 	<div id='appNav'>
 		<span>AppNav</span>
 		<IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Greeter</IndexLink>
 		<Link to="/weather" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Weather</Link>
 		<Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About</Link>
-	</div>;
-	
-module.exports = AppNav;
+	</div>
+);
