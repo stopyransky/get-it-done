93360b0d5892b67c86a115b958fccce246a6f6a4
Karol Stopyra <stopyransky@gmail.com>
2017-04-18 14:16:42 +0200

    about page updated

--- app/components/AppAbout.jsx
+++ app/components/AppAbout.jsx
@@ -1,18 +1,24 @@
 var React = require('react');
 
-// var AppAbout = React.createClass( {
-
-//     render : function() {
-
-//         return (
-//             <div id='appAbout'>
-//                 <h3> About </h3>
-//             </div>
-//         );
-//     }
-// } );
-
 // stateless functional component
-var AppAbout  = ( props ) => <h3> About </h3>;
+var AppAbout  = ( props ) => 
+	<div>
+		<h1 className="text-center"> About </h1>
+		<p>This is a weather application build on React. I have built this
+        	for The Complete React Web App Developer Course.
+      	</p>
+      	<p>
+        	Here are some of the tools I used:
+      	</p>
+     	<ul>
+        	<li>
+          		<a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        	</li>
+        	<li>
+        		<a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        	</li>
+      </ul>
+	</div>;
 
 module.exports = AppAbout;
