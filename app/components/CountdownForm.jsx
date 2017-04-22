4c3fcdbc822e4aa7f748c4289e42ea3ea236021d
Karol Stopyra <stopyransky@gmail.com>
2017-04-22 11:59:24 +0200

    CountdownForm tests passed

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -6,7 +6,7 @@ var CountdownForm  = React.createClass( {
 			e.preventDefault();
 			var strSeconds = this.refs.seconds.value;
 
-			if(strSeconds.match(/^[0-9]*$/)) {
+			if(strSeconds && strSeconds.match(/^[0-9]*$/)) {
 				this.refs.seconds.value = "";
 				this.props.onSetCountdown(parseInt(strSeconds, 10));
 			}
@@ -15,10 +15,12 @@ var CountdownForm  = React.createClass( {
 	render: function()  {
 
 		return (
-			<form ref='form' onSubmit={this.onSubmit} className="countdown-form">
-				<input type="text" ref='seconds' placeholder="Enter time in seconds"/>
-				<button className="button expanded">Start</button>
-			</form>
+			<div>
+				<form ref='form' onSubmit={this.onSubmit} className="countdown-form">
+					<input type="text" ref='seconds' placeholder="Enter time in seconds"/>
+					<button className="button expanded">Start</button>
+				</form>
+			</div>
 		);
 	}
 });
