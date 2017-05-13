38484a62ac9c3f7e2a4cbe09a5de80f839b96155
Karol Stopyra <stopyransky@gmail.com>
2017-05-13 11:31:45 +0200

    new reducers tests

--- app/rx-reducers/reducers.jsx
+++ app/rx-reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require("node-uuid");
+var moment = require("moment");
+
 export var searchTextReducer = (state = "", action) => {
 
 	switch(action.type) {
@@ -15,4 +18,36 @@ export var showCompletedReducer = (state = false, action) => {
 		default: 
 			return state;
 	}
-}
+};
+
+export var todosReducer = (state = [], action) => {
+	switch(action.type) {
+		case "ADD_TODO" :
+			return [
+				...state, // already present todos
+				{
+					id: uuid(),
+					text : action.text,
+					completed : false,
+					createdAt : moment().unix(),
+					completedAt : null
+				}
+			];
+		case "TOGGLE_TODO" : 
+			return state.map((todo) => {
+				if(todo.id === action.id) {
+					var isCompleted = !todo.completed;
+					return {
+						...todo,
+						completed : isCompleted,
+						completedAt : isCompleted ? moment().unix() : null
+					}
+				}
+				return todo;
+			});
+		default : 
+			return state;
+	}	
+};
+
+
