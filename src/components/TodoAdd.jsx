c4b7141a8413c7e38ef6e794a505cee0a2571999
Karol Stopyra <stopyransky@gmail.com>
2017-07-01 20:13:16 +0200

    fixing mobile reponsiveness

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -6,23 +6,24 @@ import * as actions from 'actions';
 
 export class TodoAdd extends React.Component {
  	
-
 	handleTodoAdd = ( newTodo ) => {
-        	var { dispatch } = this.props;
-			dispatch(actions.startAddTodo(newTodo))
+        var { dispatch } = this.props;
+		dispatch(actions.startAddTodo(newTodo))
     } 
+
+	openAddModal = () => {
+		var addModal = document.getElementById("todo-add-modal");
+		addModal.style.display = "block";
+		addModal.style.opacity = '1';
+	}
 	
 	render() {
 		
-		var {dispatch }  = this.props;
+		var { dispatch }  = this.props;
 		
 		return (
 			<div id="todo-add">
-					<div className="todo-add-button" onClick={ () => {
-						var addModal = document.getElementById("todo-add-modal");
-							addModal.style.display = "block";
-							addModal.style.opacity = '1';
-					}}>New</div>
+					<div className="todo-add-button" onClick={ this.openAddModal }>New</div>
 				<TodoModalAdd onSubmit={this.handleTodoAdd} />
 			</div>
 		);
@@ -30,5 +31,6 @@ export class TodoAdd extends React.Component {
 }
 
 var mapStateToProps = state  => state;
+// var mapDispatchToProps =  dispatch => dispatch ;
 
 export default connect(mapStateToProps)(TodoAdd);
